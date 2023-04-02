2a4c6679aa2faf60eb66efc3555dc7896b48728e
Johanna Riquelme <riquelme.johanna07@gmail.com>
2023-04-02 19:26:16 -0300

    Práctica 1.5.1

---
+++ algoritmo.js
@@ -0,0 +1,37 @@
+document. querySelector ("body").innerHTML= `<li>Receta de Tortilla</li>
+<ol>
+    <li>Conseguir: 3 Papas medianas,1 Cebolla,5 huevos, 1 taza de aceite, 100 gr de manteca, 5 gramos de provenzal,
+        5 gramos de sal y 5 gramos de pimienta negra</li>
+    <li>Pelar las papas</li>
+    <li>Cortarlas en rodajas de 1,5 cm </li>
+    <li>Cortar las rodajas en bastones de 1 cm de ancho</li>
+    <li>Pelar la cebolla</li>
+    <li>Cortar la cebolla en rodajas de 1 cm</li>
+    <li>Picar las rodajas de cebolla</li>
+    <li>Conseguir una sartén mediana</li>
+    <li>Incorporar la taza de aceite a la sartén</li>
+    <li>Calentar el aceite a fuego alto por 5 minutos</li>
+    <li>Tirar las papas al aceite</li>
+    <li>Freirlas hasta que estén doradas</li>
+    <li>Retirar las papas con una espumadera</li>
+    <li>En otra sartén incorporar la manteca</li>
+    <li>Calentarla hasta que se derrita</li>
+    <li>Agregar la cebolla</li>
+    <li>Cocinarla durante 10 minutos a fuego medio</li>
+    <li>Colocar el contenido de los huevos dentro de un bowl grande</li>
+    <li>Mezclarlo hasta que quede homogéneo</li>
+    <li>Incorporar las papas a la mezcla de huevos</li>
+    <li>Incorporar las cebollas a la mezcla de huevos</li>
+    <li>Agregar los condimentos</li>
+    <li>Mezclar por 1 minuto</li>
+    <li>Calentar una sartén preferentemente antiadherente u otro sartén a fuego medio</li>
+    <li>Poner 10 ml de aceite en el sartén</li>
+    <li>Agregar la mezcla en la sartén previamente calentada</li>
+    <li>Cuando coagule la parte inferior bajar el fuego</li>
+    <li>Cocinar por 10 minutos</li>
+    <li>Dar vuelta</li>
+    <li>Dorar la otra cara 5 minutos</li>
+    <li>Retirar</li>
+    <li>Apagar el fuego</li>
+    <img height="200 px" src="https://resizer.glanacion.com/resizer/0315JuVzNKx324D8AcSOQUAUoLw=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/EPMZH233TBBEBETIIAYLJQ57JU.jpg"
+        alt="Tortilla">`;
